317cb3563b6f9d2297aaec423e9017c0ea72ebb9
Hannah Willoughby <hannah@tcah.com>
2020-03-15 11:03:08 -0400

    Display all entries from recipes table in recipedb

--- willowtreerecipes/src/components/Recipes.vue
+++ willowtreerecipes/src/components/Recipes.vue
@@ -1,27 +1,68 @@
 <template>
 	<div>
 		<h1>Recipes :)</h1>
-		<p>{{ message }}</p>
+		<table id='all-recipes'>
+			<tr>
+				<th>Name</th>
+				<th>Description</th>
+				<th>Difficulty</th>
+				<th>Time to Complete</th>
+				<th>Date Created</th>
+			</tr>
+			<tr 
+				id='recipe-entry'
+				v-for='recipe in allRecipes'
+				:key='recipe.id'
+				:recipe='recipe'
+			>
+				<td>{{ recipe.name }}</td>
+				<td>{{ recipe.description }}</td>
+				<td>{{ recipe.difficulty }}</td>
+				<td>{{ recipe.time }}</td>
+				<td>{{ recipe.creation_date }}</td>
+			</tr>
+		</table>
 	</div>
 </template>
 
+
 <script>
-import Recipes from '@/services/Recipes'
-//import Api from '@/services/Api'
-export default {
-	data () {
+	import Recipes from '@/services/Recipes'
+	//import RecipesTableEntries from './Recipes-Table-Entries'
+
+	export default {
+	data() {
 		return {
-			message: ''
+			allRecipes: [	]
 		}
 	},
-	mounted () {
-		Recipes.loadRecipes().then( response => {
-			this.data = response.data;
-		})
-	}
+	//components: { 
+	//	RecipesTableEntries
+	//},
+  mounted () {
+    Recipes.loadRecipes().then( response => {
+      this.allRecipes = response.data;
+    })
+  }
 }
 </script>
 
 <style scoped>
+	
+#all-recipes {
+	border: 1px solid lightgrey;
+	border-collapse: collapse;
+}	
 
+#all-recipes th, td {
+	padding: 15px;
+}
+
+#all-recipes th {
+	background-color: plum;
+}
+
+#all-recipes tr:hover {
+	background-color: linen;
+}
 </style>
