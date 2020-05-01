05a7172342893c2dc4face8ab3cf337f130093f2
Hannah Willoughby <hannah@tcah.com>
2020-04-30 22:43:20 -0400

    Add footer

--- willowtreerecipes/src/views/About.vue
+++ willowtreerecipes/src/views/About.vue
@@ -1,17 +1,25 @@
 <template>
-  <div class="about">
+  <div class="home">
 		<Navigation/>
-    <h2>This is an about page</h2>
+    <h2 class='body'>This is an about page</h2>
+		<Footer/>
   </div>
 </template>
 
 <script>
 	import Navigation from '@/components/Navigation.vue'
+	import Footer from '@/components/Footer.vue'
 
 	export default {
 		name: 'About',
 		components: {
 			Navigation,
+			Footer
 		}
 	}
 </script>
+
+<style>
+  @import "../style/layout.css";
+  @import "../style/colors.css";
+</style>
