85f0452f14377986773fb20fd630140c04eb5f39
Hannah Willoughby <hannah@tcah.com>
2020-11-12 20:07:09 -0500

    Starting Fresh

--- server/src/api.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const api = express.Router();
-
-api.post('/register', (req,res,next) => {
-	res.send({message: `Thanks ${req.body.email}! Your user was registered! Have fun!`})
-})
-
-module.exports = api
